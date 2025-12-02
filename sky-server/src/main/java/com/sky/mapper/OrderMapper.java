dd6ad822d6f9b248547e2badec35e896dec14c93
ManJiacheng <man-jiacheng@163.com>
2025-12-02 17:54:31 +0800

    用户下单

---
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -0,0 +1,15 @@
+package com.sky.mapper;
+
+import com.sky.entity.Orders;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrderMapper {
+    /**
+     * 插入订单数据
+     * @param orders
+     */
+    void insert(Orders orders);
+}
